82cfcd47504e9373038e7c20ee6abb810fe5f511
FireSpirit171 <rhodyrev7@gmail.com>
2024-09-26 12:23:08 +0300

    Add full header

--- public/index.js
+++ public/index.js
@@ -1,14 +1,28 @@
+'use strict';
+
 const root = document.getElementById('root');
 
 const menuContainer = document.createElement('header');
+
 const iconContainer = document.createElement('div');
 const hrefs = document.createElement('div');
+const nameContainer = document.createElement('div');
+const signsContainer = document.createElement('div');
+const buttonContainer = document.createElement('div');
+
 iconContainer.classList.add('icon');
 hrefs.classList.add('header-hrefs');
+nameContainer.classList.add('name');
+signsContainer.classList.add('signs');
+buttonContainer.classList.add('entry-button');
+
 menuContainer.appendChild(iconContainer);
 menuContainer.appendChild(hrefs);
-root.appendChild(menuContainer);
+menuContainer.appendChild(nameContainer);
+menuContainer.appendChild(signsContainer);
+menuContainer.appendChild(buttonContainer);
 
+root.appendChild(menuContainer);
 
 const config = {
     menu: {
@@ -24,7 +38,27 @@ const config = {
             'href': '/articles',
             'text': 'Статьи',
         }
+    },
+
+    signs: {
+        Messages: {
+            'src': './images/svg/messages.svg',
+            'href': '/messages',
+        },
+        Favorites: {
+            'src': './images/svg/favorites.svg',
+            'href': '/favorites'
+        },
+        Notifications: {
+            'src': './images/svg/notifications.svg',
+            'href': '/notifications'
+        }
     }
+}
+
+const headerState = {
+    activePageLink: null,
+    headerElements: {}
 }
 
 function renderIcon() {
@@ -35,17 +69,53 @@ function renderIcon() {
     iconContainer.appendChild(imgElement);
 }
 
-function renderMenu() {
-    renderIcon();
-    Object.entries(config.menu).forEach(([_, {href, text}])=>{
+function renderMainText() {
+    const imgElement = document.createElement('img');
+    imgElement.src = './images/name.png';
+    imgElement.width = 200;
+    imgElement.height = 60;
+    nameContainer.appendChild(imgElement);
+}
+
+function renderHrefs() {
+    Object.entries(config.menu).forEach(([key, {href, text}], index)=>{
         const menuElement = document.createElement('a');
         menuElement.href = href;
         menuElement.text = text;
         menuElement.classList.add('hrefs')
 
+        if (index == 0) {
+            menuElement.classList.add('active-href');
+            headerState.activePageLink = menuElement;
+        }
+
+        headerState.headerElements[key] = menuElement;
         hrefs.appendChild(menuElement);
     });
 }
 
-renderMenu();
+function renderSigns() {
+    Object.entries(config.signs).forEach(([_, {href, src}])=>{
+        const signElement = document.createElement('a');
+        signElement.href = href;
+        signElement.innerHTML = `<img src="${src}" width="30" height="30">`;
 
+        signsContainer.appendChild(signElement);
+    })
+}
+
+function renderButton() {
+    const button = document.createElement('button');
+    button.textContent = "Войти!";
+    buttonContainer.appendChild(button);
+}
+
+function renderHeader() {
+    renderIcon();
+    renderHrefs();
+    renderMainText();
+    renderSigns();
+    renderButton();
+}
+
+renderHeader();
