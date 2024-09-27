8aceecdb1fa4e5ecb625d1ad978a2bf045538563
FireSpirit171 <rhodyrev7@gmail.com>
2024-09-27 12:34:05 +0300

    Add main photo container

--- public/index.js
+++ public/index.js
@@ -14,33 +14,47 @@ const headerCallbacks = {
     signInPage: loadSignInPage,
 }
 
+function loadMainPage() {}
+function loadMapPage() {}
+function loadArticlesPage() {}
+function loadMessagesPage() {}
+function loadFavoritesPage() {}
+function loadNotificationsPage() {}
+function loadSignInPage() {}
+
 const header = new Header(headerCallbacks);
 root.appendChild(header.getMainContainer());
 
+const mainPhotoContainer = document.createElement('div')
+function renderMainPoto(mainPhotoContainer) {
+    const hostsHrefs = document.createElement('div');
+    const findHost = document.createElement('a');
+    const beHost = document.createElement('a');
+    const searchCityForm = document.createElement('form');
+    const searchButtonDiv = document.createElement('div');
+    const search = document.createElement('input')
+    const findButton = document.createElement('button');
 
-function loadMainPage() {
+    findHost.text = "Найти хоста";
+    beHost.text = "Стать хостом";
+    findHost.href = '#';
+    beHost.href = '#';
+    searchCityForm.action = "#";
+    search.placeholder = "Search location";
+
+    searchButtonDiv.classList.add('custom-search');
+    mainPhotoContainer.classList.add('photo-container');
+    hostsHrefs.classList.add('hosts')
+    searchCityForm.classList.add('search-container');
+
+    hostsHrefs.appendChild(findHost);
+    hostsHrefs.appendChild(beHost);
+    mainPhotoContainer.appendChild(hostsHrefs);
+    searchButtonDiv.appendChild(search);
+    searchButtonDiv.appendChild(findButton);
+    searchCityForm.appendChild(searchButtonDiv);
+    mainPhotoContainer.appendChild(searchCityForm);
 }
 
-function loadMapPage() {
-
-}
-
-function loadArticlesPage() {
-
-}
-
-function loadMessagesPage() {
-
-}
-
-function loadFavoritesPage() {
-
-}
-
-function loadNotificationsPage() {
-
-}
-
-function loadSignInPage() {
-
-}
+renderMainPoto(mainPhotoContainer);
+root.appendChild(mainPhotoContainer);
