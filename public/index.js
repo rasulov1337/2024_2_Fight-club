b23dc63a5a4f9bb70411767d9307fd47c872e8d5
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-27 12:49:26 +0300

    Fix position of search button

--- public/index.js
+++ public/index.js
@@ -41,6 +41,9 @@ function renderMainPoto(mainPhotoContainer) {
     beHost.href = '#';
     searchCityForm.action = "#";
     search.placeholder = "Search location";
+    findButton.innerHTML += "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "<path d=\"M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z\" stroke=\"#808080\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/>\n" +
+        "</svg>\n"
 
     searchButtonDiv.classList.add('custom-search');
     mainPhotoContainer.classList.add('photo-container');
