33c9657e2dd92bf8921ce064e568e382ffca9ef7
FireSpirit171 <rhodyrev7@gmail.com>
2024-09-29 04:16:20 +0300

    Add registration

--- public/components/Auth/auth.js
+++ public/components/Auth/auth.js
@@ -5,11 +5,38 @@ class Auth {
         this.overlay = document.createElement('div');
         this.overlay.classList.add('overlay');
 
-        this.popup = document.createElement('div');
+        this.popup = document.createElement('form');
         this.popup.classList.add('popup');
         this.overlay.appendChild(this.popup);
 
-        document.body.classList.add('no-scroll')
+        document.body.classList.add('no-scroll');
+
+        this.currentState = 'auth';
+        this.config = {
+            'auth': {
+                message: "Войти в аккаунт",
+                inputs: {
+                    "login": "Логин (почта)",
+                    "password": "Пароль",
+                },
+                buttonText: "Войти",
+                haveAccountText: "Еще нет аккаунта?",
+                haveAccountHrefText: "Создать" 
+            },
+
+            'signup': {
+                message: "Зарегистрироваться",
+                inputs: {
+                    "name": "Полное имя",
+                    "login": "Почта",
+                    "password": "Пароль",
+                    "password2": "Повторите пароль",
+                },
+                buttonText: "Создать аккаунт",
+                haveAccountText: "Уже есть аккаунт?",
+                haveAccountHrefText: "Войти" 
+            }
+        }
 
         this.render();
     }
@@ -37,56 +64,74 @@ class Auth {
         this.popup.appendChild(imgElement);
     }
 
-    renderMessage() {
+    renderMessage(message) {
         const messageContainer = document.createElement('div');
         messageContainer.classList.add('auth-message')
-        messageContainer.textContent = "Войти в аккаунт";
+        messageContainer.textContent = message;
         this.popup.appendChild(messageContainer);
     }
 
-    renderInputs() {
-        const inputContainer = document.createElement('div');
-        inputContainer.classList.add('input-container');
-
-        const loginInput = document.createElement('input');
-        loginInput.placeholder = "Логин (почта)";
-        const passwordInput = document.createElement('input');
-        passwordInput.placeholder = "Пароль";
-
-        inputContainer.appendChild(loginInput);
-        inputContainer.appendChild(passwordInput);
-        this.popup.appendChild(inputContainer);
+    renderInputs(inputs) {
+        Object.entries(inputs).forEach(([name, placeholderText])=>{
+            const input = document.createElement('input');
+            input.classList.add('inputs');
+            input.name = name;
+            input.placeholder = placeholderText;
+            this.popup.appendChild(input);
+        })
     }
 
-    renderButton() {
+    renderButton(text) {
         const loginButton = document.createElement('button');
         loginButton.classList.add('login-button');
-        loginButton.textContent = "Войти";
+        loginButton.textContent = text;
         this.popup.appendChild(loginButton);
     }
 
-    renderHaveAccount() {
+    renderHaveAccount(text, hrefText) {
         const haveAccount = document.createElement('div');
         haveAccount.classList.add('have-account');
+
         const haveAccountText = document.createElement('p');
-        haveAccountText.textContent = "Еще нет аккаунта?";
+        haveAccountText.textContent = text;
+
         const haveAccountHref = document.createElement('a');
-        haveAccountHref.href = "#";
-        haveAccountHref.textContent = "Создать"
         haveAccountHref.classList.add('bold');
+        haveAccountHref.textContent = hrefText;
+        haveAccountHref.addEventListener('click', (e)=>{
+            e.preventDefault();
+            if (this.currentState === 'auth'){
+                this.currentState = 'signup';
+            } else {
+                this.currentState = 'auth';
+            }
+
+            this.popup.innerHTML = '';
+            this.render();
+        });
+
         haveAccount.appendChild(haveAccountText);
         haveAccount.appendChild(haveAccountHref);
+
         this.popup.appendChild(haveAccount);
     }
 
 
     render() {
+        const method = this._getMethod();
+        const config = this.config[method];
+        console.log(config);
+
         this.renderCross();
         this.renderImg();
-        this.renderMessage();
-        this.renderInputs();
-        this.renderButton();
-        this.renderHaveAccount();
+        this.renderMessage(config.message);
+        this.renderInputs(config.inputs);
+        this.renderButton(config.buttonText);
+        this.renderHaveAccount(config.haveAccountText, config.haveAccountHrefText);
+    }
+
+    _getMethod(){
+        return this.currentState;
     }
 
     getAuth() {
