76381884cd4f71317e6da033f98d2f2d6205e0fd
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-09-29 13:27:33 +0300

    Add comments for AdCard.js

--- public/components/AdCard/AdCard.js
+++ public/components/AdCard/AdCard.js
@@ -1,16 +1,19 @@
-'use strict';
+'use strict'
 
+/** Карточка объявления на главной странице */
 class AdCard {
+    /** @param {object} data - информация о карточке
+     @param {HTMLDivElement} parent - родитель, в чьем списке детей будет карточка */
     constructor(data, parent) {
         this.data = data
-        this.parent = parent;
+        this.parent = parent
 
-        this.data.mainPicture = data.pictures[0]
+        this.data.mainPicture = 'images/' + data.pictures[0]
         this.currentImgIndex = 0
         this.circles = []
 
         if (parent === undefined || parent === null) {
-            console.log("Wrong parent!")
+            console.log('Wrong parent!')
         }
     }
 
@@ -18,40 +21,54 @@ class AdCard {
         const template = Handlebars.templates['AdCard.hbs']
         this.parent.innerHTML += template(this.data)
 
-        this.parent.querySelector('.fav-btn').addEventListener('click', this.addToFavorite)
+        this.parent
+            .querySelector('.fav-btn')
+            .addEventListener('click', this.addToFavorite)
 
         setTimeout(() => {
             this.addImageScrolling()
-        }, 0)  // setTimeout ensures the code will be called AFTER browser finished rendering innerHTML new content
+        }, 0) // setTimeout ensures the code will be called AFTER browser finished rendering innerHTML new content
     }
 
+    /**
+     * Функция, которая добавляет возможность скроллинга изображений карточки как в Ozonе
+     */
     addImageScrolling() {
         const thisElement = this.parent.querySelector(`#card-${this.data.id}`)
-        const imageContainer = thisElement.querySelector('.ad-images-container');
-        const imagePaginationDiv = thisElement.querySelector('.image-pagination-div');
-        const imgElem = imageContainer.firstElementChild;
+        const imageContainer = thisElement.querySelector('.ad-images-container')
+        const imagePaginationDiv = thisElement.querySelector(
+            '.image-pagination-div'
+        )
+        const imgElem = imageContainer.firstElementChild
 
-        const imagesAmount = Math.min(this.data.pictures.length, 7)  // We must only show max amount of 7!
-        const areaFraction = imageContainer.getBoundingClientRect().width / imagesAmount
+        const imagesAmount = Math.min(this.data.pictures.length, 7) // We must only show max amount of 7!
+        const areaFraction =
+            imageContainer.getBoundingClientRect().width / imagesAmount
 
         for (let i = 0; i < imagesAmount; i++) {
-            const circle = document.createElement('div');
+            const circle = document.createElement('div')
             circle.classList.add('circle')
             this.circles.push(circle)
             imagePaginationDiv.appendChild(circle)
         }
 
-        imageContainer.addEventListener('mousemove', (e) => this.onMouseMove(e, areaFraction, imgElem))
-        imageContainer.addEventListener('mouseout', () => this.onMouseOut(imgElem))
+        imageContainer.addEventListener('mousemove', (e) =>
+            this.onMouseMove(e, areaFraction, imgElem)
+        )
+        imageContainer.addEventListener('mouseout', () =>
+            this.onMouseOut(imgElem)
+        )
 
         this.makeCircleActive(0)
     }
 
+    /**
+     * Функция, которая показывает нужную фотографию в зависимости от позиции курсора
+     */
     onMouseMove(e, areaFraction, imgElem) {
         const rect = e.target.getBoundingClientRect()
         const x = e.clientX - rect.left
-        if (x < 0)
-            return
+        if (x < 0) return
 
         const toShowIndex = Math.floor(x / areaFraction)
         if (toShowIndex === this.currentImgIndex) {
@@ -60,23 +77,33 @@ class AdCard {
 
         this.makeCircleActive(toShowIndex)
         this.currentImgIndex = toShowIndex
-        imgElem.src = this.data.pictures[toShowIndex]
+        imgElem.src = 'images/' + this.data.pictures[toShowIndex]
     }
 
+    /**
+     * Функция, которая показывает первую фотографию, когда курсор вне карточки
+     */
     onMouseOut(imgElem) {
         this.makeCircleActive(0)
         this.currentImgIndex = 0
-        imgElem.src = this.data.pictures[0]
+        imgElem.src = 'images/' + this.data.pictures[0]
     }
 
+    /**
+     * Делает кружок с индексом index выделенным. По сути пагинация для фото
+     * @param {int} index -- индекс текущего фото
+     */
     makeCircleActive(index) {
         this.circles[this.currentImgIndex].classList.remove('circle-fill')
         this.circles[index].classList.add('circle-fill')
     }
 
+    /**
+     * Вызывается при нажатии на кнопку добавить в избранное
+     */
     addToFavorite() {
-        console.log("fav btn was clicked!")
+        console.log('fav btn was clicked!')
     }
 }
 
-export default AdCard;
+export default AdCard
