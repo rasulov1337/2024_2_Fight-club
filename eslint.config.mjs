1bc56f2a63d4f870083305ca04f5c21d79cc3f19
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-12 20:10:59 +0300

    Force semicolon Reindent esling config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
-  { languageOptions: { globals: { ...globals.browser, process: 'readonly', __dirname: 'readonly' } } },
-  pluginJs.configs.recommended,
-  {
-    rules: {
-      'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
-    }
-  }
+    {
+        files: ['**/*.js', '**/*.ts'],
+        languageOptions: {
+            sourceType: 'module',
+        },
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                process: 'readonly',
+                __dirname: 'readonly',
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        },
+    },
 ];
