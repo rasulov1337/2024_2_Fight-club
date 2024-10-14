77a7aac1afd7fcdbf3a7543c5522ee915a49cbc4
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-15 01:14:48 +0300

    Add hbs hot reload support (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import handlebarsCompilePlugin from './hbs-recompile.js';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+    plugins: [handlebarsCompilePlugin()],
     server: {
         host: 'localhost',
         port: 80,
