5391571cf958d3b52512403a940a7aff36f8c48a
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-10-18 18:34:58 +0300

    Fix

--- hbs-recompile.js
+++ hbs-recompile.js
@@ -29,9 +29,7 @@ const getAllHbsFiles = (dirPath) => {
 };
 
 const recompileTemplates = () => {
-    const hbsFiles = getAllHbsFiles(
-        path.resolve(import.meta.dirname, 'components')
-    );
+    const hbsFiles = getAllHbsFiles(path.resolve(__dirname, 'components'));
     // Формируем команду для компиляции всех найденных файлов
     const hbsFilesCommand = hbsFiles.join(' '); // Собираем все файлы в строку для команды
     const command = `handlebars ${hbsFilesCommand} -f components/precompiled-templates.js`;
